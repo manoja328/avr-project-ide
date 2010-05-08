27494c4db2b7b0869a93636a9c7a28d4e91140da
frank.zhao.main <frank.zhao.main@4be8a5bc-ab0b-11de-b7bf-31c12d054fdc>
2010-05-08 17:09:17 +0000

    85

--- source/AVRProjectIDE/Properties/Resources.Designer.cs
+++ source/AVRProjectIDE/Properties/Resources.Designer.cs
@@ -111,7 +111,7 @@ namespace AVRProjectIDE.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to AVRProjectIDE_Alpha_84.
+        ///   Looks up a localized string similar to AVRProjectIDE_85.
         /// </summary>
         internal static string BuildID {
             get {
