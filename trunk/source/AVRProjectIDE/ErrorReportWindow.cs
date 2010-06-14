3ace3a9ae56b42059e3fd79da3a6a45d4be36e02
frank.zhao.main <frank.zhao.main@4be8a5bc-ab0b-11de-b7bf-31c12d054fdc>
2010-06-14 21:15:51 +0000

    update 89

--- trunk/source/AVRProjectIDE/ErrorReportWindow.cs
+++ trunk/source/AVRProjectIDE/ErrorReportWindow.cs
@@ -14,32 +14,47 @@ namespace AVRProjectIDE
         public static void Show(Exception ex, string msg)
         {
             new ErrorReportWindow(ex, msg).ShowDialog();
-            
         }
 
         private ErrorReportWindow(Exception ex, string msg)
         {
             InitializeComponent();
 
-            txtException.Text = msg + Environment.NewLine + Environment.NewLine;
-            txtException.Text += "Modified code files should have been backed up occasionally automatically, go look for them if the editor crashes." + Environment.NewLine;
-            txtException.Text += "If this is an issue or bug, please report this to http://code.google.com/p/avr-project-ide/issues/list" + Environment.NewLine;
-            txtException.Text += Environment.NewLine + "Version: " + Properties.Resources.BuildID + Environment.NewLine;
-            txtException.Text += Environment.NewLine + "Date: " + DateTime.Now.ToString("MMMM d yyyy") +Environment.NewLine;
-            txtException.Text += Environment.NewLine + "OS: " + Environment.OSVersion + Environment.NewLine;
-            txtException.Text += Environment.NewLine + "Environment Version: " + Environment.Version + Environment.NewLine;
-            txtException.Text += Environment.NewLine + "Message: " + ex.Message + Environment.NewLine;
-            txtException.Text += Environment.NewLine + "Target Site: " + ex.TargetSite + Environment.NewLine;
-            txtException.Text += Environment.NewLine + "Stack Trace: " + ex.StackTrace + Environment.NewLine;
-            txtException.Text += Environment.NewLine + "Source: " + ex.Source + Environment.NewLine;
-            string ie = ex.InnerException.ToString();
-            if (ie != null)
+            try
             {
-                if (string.IsNullOrEmpty(ie.Trim()) == false)
+                txtException.Text = msg + Environment.NewLine + Environment.NewLine;
+                txtException.Text += "Modified code files should have been backed up occasionally automatically, go look for them if the editor crashes." + Environment.NewLine;
+                txtException.Text += "If this is an issue or bug, please report this to http://code.google.com/p/avr-project-ide/issues/list" + Environment.NewLine;
+            }
+            catch { }
+            try { txtException.Text += Environment.NewLine + "Version: " + Properties.Resources.BuildID + Environment.NewLine; }
+            catch { }
+            try { txtException.Text += Environment.NewLine + "Date: " + DateTime.Now.ToString("MMMM d yyyy") + Environment.NewLine; }
+            catch { }
+            try { txtException.Text += Environment.NewLine + "OS: " + Environment.OSVersion + Environment.NewLine; }
+            catch { }
+            try { txtException.Text += Environment.NewLine + "Environment Version: " + Environment.Version + Environment.NewLine; }
+            catch { }
+            try { txtException.Text += Environment.NewLine + "Message: " + ex.Message + Environment.NewLine; }
+            catch { }
+            try { txtException.Text += Environment.NewLine + "Target Site: " + ex.TargetSite + Environment.NewLine; }
+            catch { }
+            try { txtException.Text += Environment.NewLine + "Stack Trace: " + ex.StackTrace + Environment.NewLine; }
+            catch { }
+            try { txtException.Text += Environment.NewLine + "Source: " + ex.Source + Environment.NewLine; }
+            catch { }
+            try
+            {
+                string ie = ex.InnerException.ToString();
+                if (ie != null)
                 {
-                    txtException.Text += Environment.NewLine + "Inner Exception: " + ie.Trim() + Environment.NewLine;
+                    if (string.IsNullOrEmpty(ie.Trim()) == false)
+                    {
+                        txtException.Text += Environment.NewLine + "Inner Exception: " + ie.Trim() + Environment.NewLine;
+                    }
                 }
             }
+            catch { }
         }
     }
 }
